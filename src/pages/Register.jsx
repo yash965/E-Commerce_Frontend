4e66bceaf49a14513191e585240c0572175873e2
Yash Khandelwal <82761245+yash965@users.noreply.github.com>
2025-10-13 11:19:50 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("https://book-store-uajv.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
